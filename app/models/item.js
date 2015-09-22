69d8dc2ef79602eae45abc8d8284c0e84d15d851
katiebrudos <katherine.brudos@hope.edu>
2015-09-22 00:41:06 +0000

    adding changes

--- app/models/item.js
+++ app/models/item.js
@@ -1,19 +1,27 @@
 import Ember from 'ember';
 
-var Item = Ember.Object.extend({
-
+var Item =  Ember.Object.extend({
   name: "",
   weight: 0,
   bonuses: {
     constitution: 0,
     strength: 0
-  }
-
+  },
+  bonusStrings: Ember.computed('bonuses.constitution', function() {
+    return [
+      "constitution "+"+"+this.get('bonuses.constitution')
+    ];
+  })
+  
 });
 
 Item.reopenClass({
-  createRandom: function(){
-    return Item.create({name: "The magic sword", weight:10, bonuses: {constitution:3} });
-  }
+  createRandom: function() {
+    return Item.create({
+      name: "magic sword of life", 
+      weight: 4, 
+      bonuses:{constitution: 3}});
+  } 
 });
+
 export default Item;
