4bcebe790697a66cde11a6b156c14cd6ba569f49
katiebrudos <katherine.brudos@hope.edu>
2015-11-25 17:39:41 +0000

    pushing current changes to switch to master

--- app/routes/authenticated/map.js
+++ app/routes/authenticated/map.js
@@ -1,11 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	map: Ember.inject.service(),
 	model: function(){
-		return this.store.query('world-view', {character_id: char.get('id')})
-			.then(worldViews) => {
-			this.get('map').addToWorldView(worldViews.get('firstObject'))
-		};
+
+		
 	}
 });
--- app/routes/authenticated/play.js
+++ app/routes/authenticated/play.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	model: function(params){
 		return this.store.find('character', params.character);
+		
+		//return this.store.query('world-view', {character_id: char.get('id')})
+			//.then(worldViews => {
+			//this.get('map').addToWorldView(worldViews.get('firstObject'));
+		//});
+		
 	},
 	serialize: function(model) {
 		return {character: model.get('id')};
